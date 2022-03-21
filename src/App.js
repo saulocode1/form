a01f9fe4a3a017df85ed69e1c2dac973ca46a6a5
Saulo <sauloveigr@hotmail.com>
2022-03-20 21:22:36 -0300

    refactoring  app.js file

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import { useFormik } from "formik";
-import * as yup from "yup";
-import { TextField, Button } from "@material-ui/core";
 import GlobalStyled from "./GlobalStyled";
 import FormContainer from "./Components/FormContainer/styled";
-import FormWrapper from "./Components/FormWrapper/styled";
 import MainForm from "./Components/StyledForm/";
 import SectionForm from "./Components/SectionForm/styled";
 
-const App = () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: "",
-      email: "",
-      password: "",
-    },
-    onSubmit: (values) => {
-      console.log(JSON.stringify(values));
-    },
-  });
-
-  return (
-    <>
+const App = () => (
+   <>
       <GlobalStyled />
       <SectionForm>
-        <FormContainer>
-          <MainForm />
-        </FormContainer>
+         <FormContainer>
+            <MainForm />
+         </FormContainer>
       </SectionForm>
-    </>
-  );
-};
+   </>
+);
 
 export default App;
